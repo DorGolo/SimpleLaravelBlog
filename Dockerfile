d768e901dfaa3d0e83340c6a17427daced2c1d22
DorGolo <dorgolombick@gmail.com>
2022-02-04 19:53:30 +0200

    Added dockerisation

---
+++ Dockerfile
@@ -0,0 +1,47 @@
+FROM node:latest AS node
+FROM php:7.4-fpm
+
+COPY --from=node /usr/local/lib/node_modules /usr/local/lib/node_modules
+COPY --from=node /usr/local/bin/node /usr/local/bin/node
+RUN ln -s /usr/local/lib/node_modules/npm/bin/npm-cli.js /usr/local/bin/npm
+
+RUN mkdir -p /usr/src/app
+WORKDIR /usr/src/app
+
+COPY package.json /usr/src/app
+RUN npm install
+
+COPY . /usr/src/app
+
+# Arguments defined in docker-compose.yml
+ARG user
+ARG uid
+
+# Install system dependencies
+RUN apt-get update && apt-get install -y \
+    git \
+    curl \
+    libpng-dev \
+    libonig-dev \
+    libxml2-dev \
+    zip \
+    unzip
+    
+# Clear cache
+RUN apt-get clean && rm -rf /var/lib/apt/lists/*
+
+# Install PHP extensions
+RUN docker-php-ext-install pdo_mysql mbstring exif pcntl bcmath gd
+
+# Get latest Composer
+COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+
+# Create system user to run Composer and Artisan Commands
+RUN useradd -G www-data,root -u $uid -d /home/$user $user
+RUN mkdir -p /home/$user/.composer && \
+    chown -R $user:$user /home/$user
+
+# Set working directory
+WORKDIR /var/www
+
+USER $user
---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Repositories\Interfaces\BlogPostRepositoryInterface;
+use App\Models\BlogPost;
+use Illuminate\Http\Request;
+
+class BlogController extends Controller
+{
+    private $blogPostRepository;
+  
+    public function __construct(BlogPostRepositoryInterface $blogPostRepository)
+    {
+        $this->blogPostRepository = $blogPostRepository;
+    }
+
+    public function index()
+    {
+        $posts = $this->blogPostRepository->getFeatured();
+	    return view('blog.index', [
+            'posts' => $posts,
+        ]); //returns the view with posts
+    }
+
+    public function create()
+    {
+        return view('blog.create');
+    }
+
+   
+    public function store(Request $request)
+    {
+        $newPost = BlogPost::create([
+            'title' => $request->title,
+            'body' => $request->body,
+            'user_id' => 1
+        ]);
+
+        return redirect('blog/' . $newPost->id);    }
+
+    public function show(int $blogPostId)
+    {
+        return view('blog.post', [
+            'post' => $blogPost,
+        ]); //returns the view with the post
+    }
+
+    
+    public function edit(int $blogPostId)
+    {
+        return view('blog.edit', [
+            'post' => $blogPost,
+        ]);
+    }
+
+    
+    public function update(Request $request, int $blogPostId)
+    {
+        $this->blogPostRepository->update($request::input); 
+
+        $blogPost->update([
+            'title' => $request->title,
+            'body' => $request->body
+        ]);
+
+        return redirect("blog/$blogPostId/edit");
+    }
+
+    
+    public function destroy(int $blogPostId)
+    {
+        $blogPost->delete();
+
+        return redirect('/blog');
+    }
+}
